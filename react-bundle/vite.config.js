4aa4146992946bdba76c0de90e906d499ff85992
enterkvas <alexanderbugakov66@gmail.com>
2025-11-03 11:49:57 -0800

    4: Before connecting a React component to index.html

---
+++ react-bundle/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/BurgerMenu.jsx',
+      name: 'BurgerMenu',
+      fileName: 'burger-menu',
+      formats: ['umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
